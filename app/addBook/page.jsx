7e8d5094d3ebeba2c6eca0d9515990ae0b02705d
justdoit-4head <officialsoap11@gmail.com>
2024-05-13 18:14:08 +0530

    added number validation

--- app/addBook/page.jsx
+++ app/addBook/page.jsx
@@ -112,7 +112,7 @@ export default function AddTopic() {
         <input
   onChange={(e) => setPublishYear(e.target.value)}
   value={publishYear}
-  type="text"
+  type="number"
   name="publish_year"
   id="publish_year"
   className="block lg:py-3 lg:text-lg px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
--- components/EditTopicForm.jsx
+++ components/EditTopicForm.jsx
@@ -76,7 +76,7 @@ export default function EditTopicForm({ id, title, author, publishYear }) {
               <input
         onChange={(e) => setNewpPublishYear(e.target.value)}
         value={newpPublishYear}
-        type="text"
+        type="number"
         name="publish_year"
         id="publish_year"
         className="block lg:py-3 lg:text-lg px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
