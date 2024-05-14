8b0a155502d2cc2484bc8b34349efbeb1c41fc90
justdoit-4head <officialsoap11@gmail.com>
2024-05-14 21:19:40 +0530

    envfix

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://soap:TijQZyLMYRuw3a5a@cluster0.88zmhbg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB.");
   } catch (error) {
     console.log(error);
   }
+  
 };
 
 export default connectMongoDB;
