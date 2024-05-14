f1b0e54c2b403c10cdea171d054c85857e19f507
justdoit-4head <officialsoap11@gmail.com>
2024-05-14 21:46:01 +0530

    hardcoded

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb+srv://soap:TijQZyLMYRuw3a5a@cluster0.88zmhbg.mongodb.net/");
     console.log("Connected to MongoDB.");
   } catch (error) {
     console.log(error);
